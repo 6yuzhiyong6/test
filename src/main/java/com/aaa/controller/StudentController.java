f5031ac29ef2af3e44306925ad67cc2e06824cc9
yuzhiyong@visolink.com <yuzhiyong@visolink.com>
2022-01-03 15:47:34 +0800

    发士大夫

--- src/main/java/com/aaa/controller/StudentController.java
+++ src/main/java/com/aaa/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.aaa.controller;
 import com.aaa.entity.Student;
 import com.aaa.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,11 @@ public class StudentController {
     @GetMapping("selectOne")
     public Student selectOne(Integer id) {
         return this.studentService.queryById(id);
+    }
+
+    @DeleteMapping("del")
+    public boolean delById(Integer id) {
+        return this.studentService.deleteById(id);
     }
 
 }
